4bbd1a2553f6befd923ba8a0bb073c8c3919c3ea
Oscar Gomes <dsbamba@gmail.com>
2024-09-13 11:34:07 -0300

    Atualização 13-09

---
+++ script.js
@@ -0,0 +1,15 @@
+let header = document.getElementById('header');
+let text = document.getElementById('text');
+let bigmac = document.getElementById('bigmac');
+let fritas = document.getElementById('fritas');
+let linkhome = document.getElementById('link_home');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    header.style.marginTop = value * -1.5 + 'px';
+    text.style.marginTop = value * 1.9 + 'px';
+    bigmac.style.left = value * -2.5 + 'px';
+    fritas.style.left = value * 1.5 + 'px';
+    linkhome.style.marginTop = value * 2.4 + 'px';
+})
